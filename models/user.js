6c731a47dc3a3f3e6f4521fe3f7ae3dbc87ff2e7
Oussama Benkejjane <oussama.benkejjane@gmail.com>
2019-06-10 09:32:07 +0100

    fixing unique email

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcryptjs')
 
 const UserSchema = new mongoose.Schema({
     name: String,
-    email: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
     password: { type: String, require: true}
 })
 
